1fb78a1dd8ba87861f6d8b0110ca3af66492eee7
midspace_cp <midspace@users.noreply.github.com>
2014-06-15 03:47:06 +0000

    Changed DropDown Context menu into behavior and applied across required controls.

--- Main/SEToolbox/SEToolbox/Views/WindowResourceReport.xaml.cs
+++ Main/SEToolbox/SEToolbox/Views/WindowResourceReport.xaml.cs
@@ -1,7 +1,6 @@
 ﻿namespace SEToolbox.Views
 {
     using System.Windows;
-    using System.Windows.Controls;
 
     /// <summary>
     /// Interaction logic for WindowResourceReport.xaml
@@ -12,17 +11,6 @@
         {
             this.Language = System.Windows.Markup.XmlLanguage.GetLanguage(System.Threading.Thread.CurrentThread.CurrentCulture.IetfLanguageTag);
             InitializeComponent();
-        }
-
-        // TODO: remove from code behind, into behavior?
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // Loads context menu from Button as a Drop down Menu.
-            var button = sender as Button;
-            button.ContextMenu.IsEnabled = true;
-            button.ContextMenu.PlacementTarget = button;
-            button.ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
-            button.ContextMenu.IsOpen = true;
         }
     }
 }
