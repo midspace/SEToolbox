8190c54e06abcd40d1636ccca06120bac5e67829
midspace_cp <SND\midspace_cp>
2014-11-13 21:50:22 +0000

    Updated to latest SE binaries 01.056. Updated version prior to release. Recompiled for release. Added some WIP code.

---
+++ Main/SEToolbox/SEToolbox/Views/WindowOptions.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace SEToolbox.Views
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for WindowOptions.xaml
+    /// </summary>
+    public partial class WindowOptions : Window
+    {
+        public WindowOptions()
+        {
+            this.Language = System.Windows.Markup.XmlLanguage.GetLanguage(System.Threading.Thread.CurrentThread.CurrentCulture.IetfLanguageTag);
+            InitializeComponent();
+        }
+    }
+}
