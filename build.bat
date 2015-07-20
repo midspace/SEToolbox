24b3bc191a8c66880706494c8d93290622beb07a
midspace_cp <SND\midspace_cp>
2015-07-20 10:56:10 +0000

    Reworked for x86 and x64 debugging. Removed SE binaries from source control.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /fl %~dp0\Main\SEToolbox\Build\Build.proj %* /property:ReleaseRoot=%~dp0\bin;Configuration=Release
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /fl "%~dp0Main\SEToolbox\Build\Build.proj" %* "/property:ReleaseRoot=%~dp0bin;Configuration=Release;Platform=AnyCPU"
 IF NOT [%NOPAUSE%] == [Y] PAUSE
