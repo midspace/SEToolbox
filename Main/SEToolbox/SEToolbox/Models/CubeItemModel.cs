2e5ec3a7fb15c305f469e4637c3a4b8d18683ef9
midspace_cp <midspace@users.noreply.github.com>
2015-10-26 09:46:02 +0000

    Fixed sorting of fields in Construction list. Updated Inventory reader to use newer structure, even though existing structure is still there. Some of the API's have been marked Obsolete, so it's a strong chance it has changed permanently. Updated version prior to release.

--- Main/SEToolbox/SEToolbox/Models/CubeItemModel.cs
+++ Main/SEToolbox/SEToolbox/Models/CubeItemModel.cs
@@ -1,4 +1,6 @@
-﻿namespace SEToolbox.Models
+﻿using Sandbox.Common.ObjectBuilders.ComponentSystem;
+
+namespace SEToolbox.Models
 {
     using System.Collections.ObjectModel;
     using System.Linq;
@@ -390,33 +392,21 @@
             if (Inventory == null)
                 Inventory = new ObservableCollection<InventoryEditorModel>();
 
-            var blockType = cube.GetType();
-            if (!blockType.Equals(typeof(MyObjectBuilder_CubeBlockDefinition)))
+
+            if (cube.ComponentContainer != null)
             {
-                var fields = blockType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
-                var inventoryFields = fields.Where(f => f.FieldType == typeof(MyObjectBuilder_Inventory) && f.Name != "ConstructionInventory").ToArray();
-                foreach (var field in inventoryFields)
+                var inventoryBase = cube.ComponentContainer.Components.FirstOrDefault(e => e.TypeId == "MyInventoryBase");
+
+                if (inventoryBase != null)
                 {
-                    var inventory = field.GetValue(cube) as MyObjectBuilder_Inventory;
+                    var singleInventory = inventoryBase.Component as MyObjectBuilder_Inventory;
+                    if (singleInventory != null)
+                        ParseInventory(singleInventory, definition);
 
-                    var definitionType = definition.GetType();
-                    var invSizeField = definitionType.GetField("InventorySize");
-                    var inventoryMaxVolumeField = definitionType.GetField("InventoryMaxVolume");
-                    float volumeMultiplier = 1f; // Unsure if there should be a default of 1 if there isn't a InventorySize defined.
-                    if (invSizeField != null)
-                    {
-                        var invSize = (Vector3)invSizeField.GetValue(definition);
-                        volumeMultiplier = invSize.X * invSize.Y * invSize.Z;
-                    }
-                    if (inventoryMaxVolumeField != null)
-                    {
-                        var maxSize = (float)inventoryMaxVolumeField.GetValue(definition);
-                        volumeMultiplier = MathHelper.Min(volumeMultiplier, maxSize);
-                    }
-
-                    var settings = SpaceEngineersCore.WorldResource.Checkpoint.Settings;
-                    var iem = new InventoryEditorModel(inventory, volumeMultiplier * 1000 * settings.InventorySizeMultiplier, null) { Name = field.Name, IsValid = true };
-                    Inventory.Add(iem);
+                    var aggregate = inventoryBase.Component as MyObjectBuilder_InventoryAggregate;
+                    if (aggregate != null)
+                        foreach (var field in aggregate.Inventories)
+                            ParseInventory(field as MyObjectBuilder_Inventory, definition);
                 }
             }
 
@@ -424,6 +414,30 @@
             {
                 Inventory.Add(new InventoryEditorModel(false));
             }
+        }
+
+        private void ParseInventory(MyObjectBuilder_Inventory inventory, MyObjectBuilder_CubeBlockDefinition definition)
+        {
+            if (inventory == null)
+                return;
+            var definitionType = definition.GetType();
+            var invSizeField = definitionType.GetField("InventorySize");
+            var inventoryMaxVolumeField = definitionType.GetField("InventoryMaxVolume");
+            float volumeMultiplier = 1f; // Unsure if there should be a default of 1 if there isn't a InventorySize defined.
+            if (invSizeField != null)
+            {
+                var invSize = (Vector3)invSizeField.GetValue(definition);
+                volumeMultiplier = invSize.X * invSize.Y * invSize.Z;
+            }
+            if (inventoryMaxVolumeField != null)
+            {
+                var maxSize = (float)inventoryMaxVolumeField.GetValue(definition);
+                volumeMultiplier = MathHelper.Min(volumeMultiplier, maxSize);
+            }
+
+            var settings = SpaceEngineersCore.WorldResource.Checkpoint.Settings;
+            var iem = new InventoryEditorModel(inventory, volumeMultiplier * 1000 * settings.InventorySizeMultiplier, null) { Name = inventory.InventoryFlags.ToString(), IsValid = true };
+            Inventory.Add(iem);
         }
     }
 }
